6be08e8c00b7b4eacb74320b2e7810838e8eddae
jiangtf78678 <13651032592@163.com>
2020-08-09 21:37:24 +0800

    git no1 merge

--- src/main/java/com/jiangtf/demo/entity/BaseEntity.java
+++ src/main/java/com/jiangtf/demo/entity/BaseEntity.java
@@ -4,10 +4,12 @@ package com.jiangtf.demo.entity;
 
 public class BaseEntity {
     private  String id;
+    //jtf
 
     public String getId() {
         return id;
     }
+
 
     public void setId(String id) {
         this.id = id;
