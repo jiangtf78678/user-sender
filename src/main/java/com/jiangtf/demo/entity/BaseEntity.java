4fd6b158d57a1e5c4f1e090d45676ebea2a6d196
jiangtf78678 <13651032592@163.com>
2020-08-20 22:16:21 +0800

    流划

--- src/main/java/com/jiangtf/demo/entity/BaseEntity.java
+++ src/main/java/com/jiangtf/demo/entity/BaseEntity.java
@@ -1,34 +1,19 @@
 package com.jiangtf.demo.entity;
 
 
+import java.io.Serializable;
 
-public class BaseEntity {
+public class BaseEntity implements Serializable {
     //明天放假放假
     private  String id;
-    //jtf
+
 
     public String getId() {
         return id;
     }
-    //33333
 
-
-
-    //33333
-
-
-    /////444444444444444444444
-
-    //分支提交了两次，看看这个会不会合并到主分支上
-
-    /////444444444444444444444
-
-
-
-    //删除掉了刚才的测试点。试一下另外一种方案。
-
-    //public 11点半
     public void setId(String id) {
         this.id = id;
     }
+
 }
